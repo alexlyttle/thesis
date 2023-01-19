8aea051e77964c9985df69f6fa5a03605c2f2cb5
alexlyttle <a.j.lyttle@outlook.com>
2023-01-19 17:19:16 +0000

    Add FGONG variable names

---
+++ src/thesis/io/fgong.py
@@ -0,0 +1,60 @@
+GLOBAL_KEYS = [
+    "M",
+    "R",
+    "L",
+    "initial_Z",
+    "initial_X",
+    "alpha_MLT",
+    "phi_MLT",
+    "xi_MLT",
+    "beta_surf",
+    "lambda_surf",
+    "d2lnPc_dlnr2",
+    "d2lnrhoc_dlnr2",
+    "star_age",
+    "Teff",
+    "G"
+]
+
+VARIABLE_KEYS = [
+    "R_minus_r",  # from surface to centre
+    "ln_q",
+    "T",
+    "P",
+    "rho",
+    "X",
+    "L_r",
+    "kappa",
+    "eps",
+    "Gamma_1",
+    "nabla_ad",
+    "delta",
+    "cp",
+    "inverse_mu_e",
+    "AA",
+    "nuc_X_rate",
+    "Z",
+    "r",  # from centre to surface
+    "eps_g",
+    "L_g",
+    "X_He3",
+    "X_C12",
+    "X_C13",
+    "X_N14",
+    "X_O16",
+    "dGamma_1_rho",
+    "dGamma_1_P",
+    "dGamma_1_Y",
+    "X_H2",
+    "X_He4",
+    "X_Li7",
+    "X_Be7",
+    "X_N15",
+    "X_O17",
+    "X_O18",
+    "X_Ne20",
+]
+
+
+
+# How about a star format where only certain variables are included?
