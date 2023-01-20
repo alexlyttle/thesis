ad3072f962689e021d21c55f468adac02da3f851
alexlyttle <a.j.lyttle@outlook.com>
2023-01-20 17:16:03 +0000

    Add load and save dataset

--- src/thesis/io/utils.py
+++ src/thesis/io/utils.py
@@ -1,4 +1,12 @@
+import h5py
 import xarray as xr
+import numpy as np
+
+def decode(b):
+    """Decode numpy bytes string to UTF-8."""
+    if isinstance(b, np.bytes_):
+        return b.decode('UTF-8')
+    return b 
 
 def structured_array_to_attrs(array):
     return {key: array[key] for key in array.dtype.names}
@@ -17,3 +25,9 @@ def tomso_to_dataset(tomso_log, dim=None):
         dims = (dim,)
     data_vars = structured_array_to_vars(tomso_log.data, coords=coords, dims=dims)
     return xr.Dataset(data_vars, attrs=attrs)
+
+def load_dataset(filename):
+    xr.load_dataset(filename, engine="h5netcdf")
+
+def save_dataset(dataset: xr.Dataset, filename, **kwargs):
+    dataset.to_netcdf(filename, engine="h5netcdf", **kwargs)
